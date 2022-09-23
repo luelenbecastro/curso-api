6e3cbc2426186f72828065c98ac71535ef1251c7
luelenbecastro <thomazluelen@gmail.com>
2022-09-23 10:21:00 -0300

    projeto iniciado

---
+++ src/test/java/br/com/luelen/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luelen.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
