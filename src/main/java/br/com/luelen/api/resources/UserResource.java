7d8c3d1fad861767111a1e923c16cfdc15e2e88b
luelenbecastro <thomazluelen@gmail.com>
2022-09-23 17:02:01 -0300

    retornando dto para user resource

--- src/main/java/br/com/luelen/api/resources/UserResource.java
+++ src/main/java/br/com/luelen/api/resources/UserResource.java
@@ -1,7 +1,8 @@
 package br.com.luelen.api.resources;
 
-import br.com.luelen.api.domain.Usuario;
+import br.com.luelen.api.domain.dto.UserDTO;
 import br.com.luelen.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Usuario> findById(@PathVariable Integer id) {
-
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id),UserDTO.class));
     }
 }
