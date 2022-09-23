ecfa1f4e809d909ff040b1eb31ab9e6dff2911c8
luelenbecastro <thomazluelen@gmail.com>
2022-09-23 13:02:51 -0300

    renomeado a classe User para não dar erro

--- src/main/java/br/com/luelen/api/domain/Usuario.java
+++ src/main/java/br/com/luelen/api/domain/Usuario.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class User {
+public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
