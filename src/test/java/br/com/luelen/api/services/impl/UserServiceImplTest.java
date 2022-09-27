a33b195e8c19e8268b9e71133d75e70285a3d230
luelenbecastro <thomazluelen@gmail.com>
2022-09-27 14:39:27 -0300

    teste para classe user service metodo create com sucesso

--- src/test/java/br/com/luelen/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/luelen/api/services/impl/UserServiceImplTest.java
@@ -17,6 +17,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -91,7 +92,17 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSucess() {
+        when(repository.save(any())).thenReturn(user);
+
+        Usuario response = service.create(userDTO);
+
+        assertNotNull(response);
+        assertEquals(Usuario.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
