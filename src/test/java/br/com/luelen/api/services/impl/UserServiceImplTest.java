8d7880ae80d44a195ef38bcc9a6beb9150652fd8
luelenbecastro <thomazluelen@gmail.com>
2022-09-27 14:24:47 -0300

    teste para classe user service metodo finAll

--- src/test/java/br/com/luelen/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/luelen/api/services/impl/UserServiceImplTest.java
@@ -12,10 +12,11 @@ import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
-import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -24,8 +25,11 @@ class UserServiceImplTest {
 
     public static final Integer ID      = 1;
     public static final String NAME     = "Luelen";
-    public static final String EMAIL    = "luelen@gmail.com";
+    public static final String EMAIL    = "thomaz@gmail.com";
     public static final String PASSWORD = "123";
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
+    public static final int INDEX = 0;
+
     @InjectMocks
     private UserServiceImpl service;
 
@@ -38,7 +42,6 @@ class UserServiceImplTest {
     private Usuario user;
     private UserDTO userDTO;
     private Optional<Usuario> optionalUsuario;
-
 
     @BeforeEach
     void setUp() {
@@ -58,20 +61,33 @@ class UserServiceImplTest {
         assertEquals(NAME, response.getName());
         assertEquals(EMAIL, response.getEmail());
     }
-    @Test
-    void whenFindByIdThenReturnAnObjectNotFoundException() {
-        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException("Objeto não encontado"));
 
-        try{
-            service.findById(ID);
-        } catch (Exception ex) {
-            assertEquals(ObjectNotFoundException.class, ex.getClass());
-            assertEquals("Objeto não encontado",ex.getMessage());
-        }
+    @Test
+    void whenFindbyIDThenReturnAnObjectNotFoundException() {
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
+
+                try {
+                    service.findById(ID);
+                } catch (Exception ex) {
+                    assertEquals(ObjectNotFoundException.class, ex.getClass());
+                    assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
+                }
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() {
+        when(repository.findAll()).thenReturn(List.of(user));
+
+        List<Usuario> response = service.findAll();
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(Usuario.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NAME, response.get(INDEX).getName());
+        assertEquals(EMAIL, response.get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.get(INDEX).getPassword());
     }
 
     @Test
