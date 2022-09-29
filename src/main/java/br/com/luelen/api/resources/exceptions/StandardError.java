0de178094c13e97e1e70990bc517f0900c1dd0d2
luelenbecastro <thomazluelen@gmail.com>
2022-09-29 16:05:40 -0300

    removendo anotações não utilizadas

--- src/main/java/br/com/luelen/api/resources/exceptions/StandardError.java
+++ src/main/java/br/com/luelen/api/resources/exceptions/StandardError.java
@@ -2,12 +2,11 @@ package br.com.luelen.api.resources.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
+
 @AllArgsConstructor
 public class StandardError {
 
